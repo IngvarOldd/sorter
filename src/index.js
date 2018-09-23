925b9e9801afd71617fcb9c0245f3d991e5cd730
IngvarOldd <lyushinskij.i.a@gmail.com>
2018-09-23 19:27:20 +0300

    third task 100%

--- src/index.js
+++ src/index.js
@@ -1,30 +1,49 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
+    this.compareFunction = function (left, right) { 
+      return left - right 
+      };
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return this.arr.length;
   }
 
   toArray() {
     // your implementation
+    return this.arr;
   }
 
   sort(indices) {
     // your implementation
+    var b = [];
+    for (let i = 0; i < indices.length; i++) {
+      b.push(this.arr[indices[i]]);
+    }
+    b.sort(this.compareFunction); 
+    indices.sort(function(left, right){ 
+      return left - right 
+    }); 
+    for (let i = 0; i < indices.length; i++) {
+      this.arr.splice(indices[i], 1, b[i]);     
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
